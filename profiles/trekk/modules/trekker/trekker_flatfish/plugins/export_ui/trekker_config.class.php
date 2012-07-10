d3712df3aba0eaee5f990fedd7ed080b191a9c6a
Tim Loudon <tloud365@gmail.com>
2012-07-10 00:51:22 -0400

    WIP
    add migrate_extras to make file + add deps to flatfish .info
    
    apparently the ctools magick is working!!!1!!!1!1!
    
    updated most of Migrate functionality to work w/ new schema, etc

--- profiles/trekk/modules/trekker/trekker_flatfish/plugins/export_ui/trekker_config.class.php
+++ profiles/trekk/modules/trekker/trekker_flatfish/plugins/export_ui/trekker_config.class.php
@@ -3,23 +3,21 @@
  *
  */
 class trekker_config extends ctools_export_ui {
-
   function list_form(&$form, &$form_state) {
     parent::list_form($form, $form_state);
-
-    // Register migrations for enabled configs
+    // Register migrations for each enabled configs
     foreach ($form_state['object']->items as $name => $config) {
-
-      if (!isset($config->disabled)) {
-        foreach ($config->config as $component => $def) {
-          if (!strcasecmp('media', $component)) { $class = 'MediaMigration';
-          }
-          else {
-            $class = 'NodeMigration';
-          }
-          $args = array('machine_name' => $config->machine_name, 'component' => $component);
-          Migration::registerMigration($class, strtolower($config->machine_name . '_' . $component), $args);
-        }        
+      if (isset($config->disabled)) {
+        continue;
+      }
+      foreach ($config->config['components'] as $component => $def) {
+        if (!strcasecmp('media', $component)) { $class = 'MediaMigration';
+        }
+        else {
+          $class = 'NodeMigration';
+        }
+        $args = array('machine_name' => $config->machine_name, 'component' => $component);
+        Migration::registerMigration($class, strtolower($config->machine_name . '_' . $component), $args);
       }
     }
   }
