ae0c84b71303be697617a66206b8fb60be9dbb3d
Tim Loudon <tloud365@gmail.com>
2012-07-30 22:05:27 -0400

    UGH, this allows for multiple content types to be migrated at once
    since the config.yml, schema.yml settings still need a reworking--the
    relationship between ctools exportable and Migration classes was
    one-to-many which borked things w/ multiple content types.
    
    eg;
    previous-state:
      Article = node Migration + media Migration
      Page = node Migration + media Migration
      and the media Migrations would conflict
    
    current-state:
      Article = node Migration
      Page = node Migration
      Flatfish = media Migration (in the Migrate UI this shows up as
    flatfish_media...which seemed better than media_media)
    
    future-state:
      fix the config/schema setup
      hopefully this will make it a little easier to write a clean design. right
    now, machine_name + _ + component name = migration name, so hacking
    machine_name or component affects a bit of the code.

--- profiles/trekk/modules/trekk_flatfish/plugins/export_ui/trekk_flatfish_config.class.php
+++ profiles/trekk/modules/trekk_flatfish/plugins/export_ui/trekk_flatfish_config.class.php
@@ -8,15 +8,17 @@ class trekk_flatfish_config extends ctools_export_ui {
     // Register migrations for each enabled configs
     foreach ($form_state['object']->items as $name => $config) {
       if ((isset($config->disabled) && FALSE == $config->disabled) || !isset($config->disabled)) {
-        foreach ($config->config['components'] as $component => $def) {
-          if (!strcasecmp('media', $component)) { $class = 'MediaMigration';
-          }
-          else {
-            $class = 'NodeMigration';
-          }
-          $args = array('machine_name' => $config->machine_name, 'component' => $component);
-          Migration::registerMigration($class, strtolower($config->machine_name . '_' . $component), $args);
+        // TODO rewrite this, is component necessary?
+        if ('Flatfish' == $config->machine_name) {
+          $class = 'MediaMigration';
+          $component = 'media';
         }
+        else {
+          $class = 'NodeMigration';
+          $component = 'node';
+        }
+        $args = array('machine_name' => $config->machine_name, 'component' => $component);
+        Migration::registerMigration($class, strtolower($config->machine_name . '_' . $component), $args);
       }
     }
   }
